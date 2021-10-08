0191327f6574fb2622f4b2878885c03d2befe1c5
Robson Rodrigues <robson1821@gmail.com>
2021-10-08 09:39:01 -0300

    Legenda Tabela Periódica
    Legenda da Tabela Periódica adicionada

--- assets/script.js
+++ assets/script.js
@@ -19,32 +19,58 @@ request.onload = function() {
 
 function mostrarEmpresa(jsonObj) {
     var empresa = jsonObj['Empresas'];
+    var legendTable = document.createElement('div');
+    var grupo = Array.from(new Set(empresa.map(function(empresa){ 
+        return empresa.Grupo;
+     })));
+     var cor = Array.from(new Set(empresa.map(function(empresa){
+        return empresa.Cor;
+     })));
 
+    // Criar a tabela no HTML
     for (var i = 0; i < empresa.length; i++){
         var periodicElement = document.createElement('div');
         var periodicElementInner = document.createElement('div');
         var title = document.createElement('div');
         var description = document.createElement('div');
 
-        var attrArray = [periodicElement];
-
         periodicElement.classList.add("periodic-element");
         periodicElement.setAttribute("data-group", empresa[i].Grupo);
         periodicElementInner.setAttribute("class", "periodic-element-inner");
-        periodicElement.setAttribute("id", "teste3");
         title.setAttribute("class", "title");
         description.setAttribute("class", "description");
-
         section.setAttribute("class", "periodic-table");
+        periodicElement.style.setProperty("--color", empresa[i].Cor);
 
+        periodicElement.style.gridColumn = empresa[i].Coluna;
+        periodicElement.style.gridRow = empresa[i].Linha;
         title.textContent = empresa[i].Abreviacao;
         description.textContent = empresa[i].Nome;
 
         periodicElementInner.appendChild(title);
         periodicElementInner.appendChild(description);
-
         periodicElement.appendChild(periodicElementInner);
+        section.appendChild(periodicElement);
+    }
 
-        section.appendChild(periodicElement); 
+    //Criar a Legenda no HTML
+    for (var j = 0; j < grupo.length; j++){
+        var legendTableWrap = document.createElement('div');
+        var legendTableMarker = document.createElement('span');
+        var legendTableText = document.createElement('span');
+
+        legendTable.setAttribute("class", "legend-table");
+        legendTableWrap.setAttribute("class", "legend-table-wrap");
+        legendTableMarker.setAttribute("class", "legend-table__marker " + grupo[j].replace(/\s+/g, '-').toLowerCase());
+        legendTableText.setAttribute("class", "legend-table__text");
+        legendTableMarker.style.setProperty("--color", cor[j]);
+
+        legendTableText.textContent = grupo[j];
+
+        legendTableWrap.appendChild(legendTableMarker);
+        legendTableWrap.appendChild(legendTableText);
+        legendTable.appendChild(legendTableWrap);
+
+        legendTableHtml.appendChild(legendTable);
     }
 }
